174e8d9bd42df9739bb1bff5a5ce520a8671dfc2
austiniqer <63011075@kmitl.ac.th>
2021-08-07 09:41:48 +0700

    function

---
+++ function.py
@@ -0,0 +1,8 @@
+# def sayHello(fisrtname, lastname) :
+# 	print('hello', fisrtname, lastname)
+# sayHello(lastname='Maha', fisrtname='Abdulahakim')
+
+# lambda expression
+
+x = lambda i, j : i ** j
+print(x(2,3))
