f2a9cbd230664307031173aa31b8226edcadeb66
austiniqer <63011075@kmitl.ac.th>
2021-08-07 12:58:25 +0700

    inher

--- class.py
+++ class.py
@@ -1,12 +1,29 @@
 class Employee :
-	def __init__(self, name, age) :
+	def __init__(self, name, age, salary) :
 		self.name = name
 		self.age = age
+		self.salary = salary
+
 	def detail(self) -> str :
 		print('Name:',self.name, self.age,'years old')
+
 	def getAge(self) :
 		print('10')
 
-user1 = Employee('austiniqer',19)
-user1.detail()
-user1.getAge()
+	def getSalary(self) :
+		print('10')
+	
+	def credential(self) -> str:
+		return 'Name:',self.name,'Age:',self.age,'Salary:',self.salary
+
+class Programmer(Employee):
+	def __init__(self, name, age, salary,skill):
+		super().__init__(name, age, salary)
+		self.skill = skill
+
+	def showSkill(self):
+		print(self.skill)
+	
+user1 = Programmer('austiniqer',19,14000.99,'C, Java, JavaScript, Python')
+print(user1.credential())
+
