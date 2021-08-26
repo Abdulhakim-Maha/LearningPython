c701ce92c4f2127b50d0ba4d1cdac3b5f4e1322c
austiniqer <63011075@kmitl.ac.th>
2021-08-26 23:02:55 +0700

    Python2 All done!

--- Python2/4.py
+++
@@ -1,5 +0,0 @@
-def hbd(age):
-	pass
-
-year = int(input('Enter year : '))
-print('saimai is just {}, in base {}!'.format(year/2, ))
---
+++ Python2/63011075_Lab2_4.py
@@ -0,0 +1,6 @@
+def hbd(age):
+	pass
+	return 'saimai is just 2{}, in base {}!'.format(age % 2,age // 2)
+
+year = int(input('Enter year : '))
+print(hbd(year))
