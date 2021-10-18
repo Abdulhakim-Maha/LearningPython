79b2362b4f67b6b24275963ac765246a9b5a22c1
austiniqer <63011075@kmitl.ac.th>
2021-10-19 06:22:16 +0700

    Happy Number

---
+++ Recursion/sum_of_digit.py
@@ -0,0 +1,14 @@
+def happynumber(n):
+	sum = 0
+	while n > 0 :
+		sum += (n % 10) ** 2
+		n //= 10
+	if sum >= 0 and sum <= 9:
+		return True if sum == 1 else False
+	else:
+		return happynumber(sum)
+
+
+if __name__ == '__main__':
+	inp = int(input('Enter numter : '))
+	print(happynumber(inp))
