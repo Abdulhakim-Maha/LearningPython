d56cd484b695bc06820e5bb9f1d289ac0bbe3d08
austiniqer <63011075@kmitl.ac.th>
2021-10-29 20:39:33 +0700

    -

---
+++ Realtime-chat/client/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/card/Card";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+
+  console.log(user);
+
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <Navbar/>
+          <Card/>
+          <span className='username'>{user}</span>
+        </>
+      ) : (
+        <div className="login">
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={(e) => setUser(username)}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
