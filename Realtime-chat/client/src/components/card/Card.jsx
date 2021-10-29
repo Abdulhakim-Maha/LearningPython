1e7ee644efa942dd9d83baff9fa54cb47e3a72d4
austiniqer <63011075@kmitl.ac.th>
2021-10-29 21:42:45 +0700

    extra

--- Realtime-chat/client/src/components/card/Card.jsx
+++ Realtime-chat/client/src/components/card/Card.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import './Card.css'
+import React, { useState } from "react";
+import "./Card.css";
+import Heart from "../../img/heart.svg";
+import HeartFilled from "../../img/heartFilled.svg";
+import Comment from "../../img/comment.svg";
+import Share from "../../img/share.svg";
+import Info from "../../img/info.svg";
 
-const Card = () => {
-	return (
-		<div className='card'>
-			card			
-		</div>
-	)
-}
+const Card = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const handleNotification = () =>{
+	  setLiked(true)
+  }
+  return (
+    <div className="card">
+      <div className="info">
+        <img src={post.userImg} alt="" className="userImg" />
+        <span>{post.fulname}</span>
+      </div>
+      <img src={post.postImg} alt="" className="postImg" />
+      <div className="interaction">
+        {liked ? (
+          <img src={HeartFilled} alt="" className="cardIcon" />
+        ) : (
+          <img src={Heart} alt="" className="cardIcon" onClick={handleNotification} />
+        )}
+        <img src={Comment} alt="" className="cardIcon" />
+        <img src={Share} alt="" className="cardIcon" />
+        <img src={Info} alt="" className="cardIcon infoIcon" />
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- Realtime-chat/client/src/components/navbar/Navbar.jsx
+++ Realtime-chat/client/src/components/navbar/Navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import Notification from "../../img/notification.svg";
+import Message from "../../img/message.svg";
+import Settings from "../../img/settings.svg";
 
 const Navbar = () => {
-	return (
-		<div className='navbar'>
-			navbar
-		</div>
-	)
-}
+  return (
+    <div className="navbar">
+      <span className="logo">Mine App</span>
+      <div className="icons">
+        <div className="icon">
+          <img src={Notification} className="iconImg" alt="" />
+          <div className="counter">2</div>
+        </div>
+        <div className="icon">
+          <img src={Message} className="iconImg" alt="" />
+          <div className="counter">2</div>
+        </div>
+        <div className="icon">
+          <img src={Settings} className="iconImg" alt="" />
+          <div className="counter">2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
